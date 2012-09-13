eba56ccd844bca27d1b4a5e94df50d50ee3d2420
Vvinothkum <vvinothkum@localhost>
2012-09-13 06:01:41 +0000

    Vss2Git

--- CoincoDevMVC4/Coinco.SMS.Website/Models/LineProperty.cs
+++ CoincoDevMVC4/Coinco.SMS.Website/Models/LineProperty.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data;
+using System.Web.Mvc;
+using Coinco.SMS.AXWrapper;
+using StructureMap;
 
 namespace Coinco.SMS.Website.Models
 {
@@ -10,7 +14,7 @@ namespace Coinco.SMS.Website.Models
         public string LinePropertyCode { get; set; }
         public string LinePropertyDescription { get; set; }
 
-        public List<LineProperty> LinePropertyList { get; set; } 
+        public SelectList LinePropertyList { get; set; } 
 
         public LineProperty()
         {
@@ -21,6 +25,35 @@ namespace Coinco.SMS.Website.Models
         {
             this.LinePropertyCode = linePropertyCode;
             this.LinePropertyDescription = linePropertyDescription;
+        }
+
+
+        public IEnumerable<LineProperty> GetLineProperty(string userName)
+        {
+            IAXHelper axHelper = ObjectFactory.GetInstance<IAXHelper>();
+            List<LineProperty> LinePropertyList = new List<LineProperty>();
+            try
+            {
+                DataTable resultTable = axHelper.GetLinePropertyList(userName);
+
+
+                foreach (DataRow row in resultTable.Rows)
+                {
+                    LineProperty LinePropertyObject = new LineProperty();
+                    LinePropertyObject.LinePropertyCode = row["LinePropertyCode"].ToString();
+                    LinePropertyObject.LinePropertyDescription = row["LinePropertyName"].ToString();
+
+                    LinePropertyList.Add(LinePropertyObject);
+
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+
+            }
+            return LinePropertyList.AsEnumerable<LineProperty>();
+
         }
     }
 }
