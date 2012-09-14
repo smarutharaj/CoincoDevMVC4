5a3e76d90aaac02a81f535b16a410f7db705279c
Csubramani <csubramani@localhost>
2012-09-14 10:49:48 +0000

    Vss2Git

--- CoincoDevMVC4/Coinco.SMS.Website/Controllers/RepairLinesController.cs
+++ CoincoDevMVC4/Coinco.SMS.Website/Controllers/RepairLinesController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Coinco.SMS.Website.Models;
+using Telerik.Web.Mvc;
 
 namespace Coinco.SMS.Website.Controllers
 {
@@ -13,8 +15,12 @@ namespace Coinco.SMS.Website.Controllers
 
         public ActionResult RepairLines()
         {
+
+            RepairType repairType = new RepairType();
+            repairType.ConditionList= repairType.GetCondtions("");
+
             TempData.Keep();
-            return View();
+            return View("RepailLines", repairType);
         }
 
     }
