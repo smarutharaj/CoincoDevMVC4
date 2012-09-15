166dd402558b13b0657410c37fecfe33ebfea508
Vvinothkum <vvinothkum@localhost>
2012-09-15 10:07:03 +0000

    Vss2Git

--- CoincoDevMVC4/Coinco.SMS.Website/Models/Location.cs
+++ CoincoDevMVC4/Coinco.SMS.Website/Models/Location.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data;
+using System.Web.Mvc;
+using Coinco.SMS.AXWrapper;
+using StructureMap;
 
 namespace Coinco.SMS.Website.Models
 {
@@ -23,6 +27,32 @@ namespace Coinco.SMS.Website.Models
             this.LocationId = locationId;
             this.LocationName = locationName;
             this.LocationQty = locationQty;
+        }
+
+        public List<Location> GetLocations(string itemNumber, string site, string warehouse, string userName)
+        {
+            IAXHelper axHelper = ObjectFactory.GetInstance<IAXHelper>();
+            List<Location> wareList = new List<Location>();
+
+            try
+            {
+                DataTable resultTable = axHelper.GetLocations(itemNumber, site, warehouse, userName);
+
+                foreach (DataRow row in resultTable.Rows)
+                {
+                    Location locationObject = new Location();
+                    locationObject.LocationId = row["LocationID"].ToString();
+                    locationObject.LocationQty = row["PhysicalQty"].ToString();
+                    wareList.Add(locationObject);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return wareList;
+
         }
     }
 }
