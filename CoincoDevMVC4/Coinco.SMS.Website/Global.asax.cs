4633a32c3ce24ffed946fc6fb9cc5a6852a2f291
Vvinothkum <vvinothkum@localhost>
2012-09-25 18:25:06 +0000

    Vss2Git

--- CoincoDevMVC4/Coinco.SMS.Website/Global.asax.cs
+++ CoincoDevMVC4/Coinco.SMS.Website/Global.asax.cs
@@ -20,7 +20,7 @@ namespace Coinco.SMS.Website
         {
             AreaRegistration.RegisterAllAreas();
 
-            WebApiConfig.Register(GlobalConfiguration.Configuration);
+            //WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             //BundleConfig.RegisterBundles(BundleTable.Bundles);
@@ -37,48 +37,7 @@ namespace Coinco.SMS.Website
             );
 
         }
-        protected void Session_Start()
-        {
-            //if (Context.Session != null)
-            //{
-            //    if (Context.Session.IsNewSession)
-            //    {
-            //        string sCookieHeader = Request.Headers["Cookie"];
-            //        if ((null != sCookieHeader) && (sCookieHeader.IndexOf("ASP.NET_SessionId") >= 0))
-            //        {
-            //            // intercept current route
-            //            HttpContextBase currentContext = new HttpContextWrapper(HttpContext.Current);
-            //            RouteData routeData = RouteTable.Routes.GetRouteData(currentContext);
-
-
-            //            var domainRoute = routeData.Route;
-
-
-            //            // substitute route values
-            //            routeData.Values["controller"] = "WorkOrder";
-            //            routeData.Values["action"] = "ServiceOrderWithHistory";
-
-
-            //            // Clear the error on server.
-            //            Server.ClearError();
-            //            Response.Clear();
-
-
-            //            // Call target Controller and pass the routeData.
-            //            IController myController = new Controllers.WorkOrderController();
-            //            myController.Execute(new RequestContext(new HttpContextWrapper(Context), routeData));
-
-
-            //            Response.Flush();
-            //            Response.End();
-            //        }
-            //    }
-            //}
-        }
-        protected void Session_End(object sender, EventArgs e)
-        {
-            
-        }
+       
 
     }
 }
