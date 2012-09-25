07811713f958d67c59192cc955a73a3974051bdf
Vvinothkum <vvinothkum@localhost>
2012-09-25 16:27:59 +0000

    Vss2Git

--- CoincoDevMVC4/Coinco.SMS.Website/Global.asax.cs
+++ CoincoDevMVC4/Coinco.SMS.Website/Global.asax.cs
@@ -39,41 +39,41 @@ namespace Coinco.SMS.Website
         }
         protected void Session_Start()
         {
-            if (Context.Session != null)
-            {
-                if (Context.Session.IsNewSession)
-                {
-                    string sCookieHeader = Request.Headers["Cookie"];
-                    if ((null != sCookieHeader) && (sCookieHeader.IndexOf("ASP.NET_SessionId") >= 0))
-                    {
-                        // intercept current route
-                        HttpContextBase currentContext = new HttpContextWrapper(HttpContext.Current);
-                        RouteData routeData = RouteTable.Routes.GetRouteData(currentContext);
+            //if (Context.Session != null)
+            //{
+            //    if (Context.Session.IsNewSession)
+            //    {
+            //        string sCookieHeader = Request.Headers["Cookie"];
+            //        if ((null != sCookieHeader) && (sCookieHeader.IndexOf("ASP.NET_SessionId") >= 0))
+            //        {
+            //            // intercept current route
+            //            HttpContextBase currentContext = new HttpContextWrapper(HttpContext.Current);
+            //            RouteData routeData = RouteTable.Routes.GetRouteData(currentContext);
 
 
-                        var domainRoute = routeData.Route;
+            //            var domainRoute = routeData.Route;
 
 
-                        // substitute route values
-                        routeData.Values["controller"] = "WorkOrder";
-                        routeData.Values["action"] = "ServiceOrderWithHistory";
+            //            // substitute route values
+            //            routeData.Values["controller"] = "WorkOrder";
+            //            routeData.Values["action"] = "ServiceOrderWithHistory";
 
 
-                        // Clear the error on server.
-                        Server.ClearError();
-                        Response.Clear();
+            //            // Clear the error on server.
+            //            Server.ClearError();
+            //            Response.Clear();
 
 
-                        // Call target Controller and pass the routeData.
-                        IController myController = new Controllers.WorkOrderController();
-                        myController.Execute(new RequestContext(new HttpContextWrapper(Context), routeData));
+            //            // Call target Controller and pass the routeData.
+            //            IController myController = new Controllers.WorkOrderController();
+            //            myController.Execute(new RequestContext(new HttpContextWrapper(Context), routeData));
 
 
-                        Response.Flush();
-                        Response.End();
-                    }
-                }
-            }
+            //            Response.Flush();
+            //            Response.End();
+            //        }
+            //    }
+            //}
         }
         protected void Session_End(object sender, EventArgs e)
         {
